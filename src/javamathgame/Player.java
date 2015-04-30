2d2973ec44a122e88cb1581462e940d44c46c383
Alessio Monegat <alessio.monegat@gmail.com>
2015-04-30 09:46:02 +0200

    Added Auth Function, not working yet

--- src/javamathgame/Player.java
+++ src/javamathgame/Player.java
@@ -10,13 +10,43 @@ package javamathgame;
  * @author muster
  */
 public class Player {
-    private String name;
-    private int exp;
-    private int lvl;
-    private int cash;
-    private int highscore;
+    String nickname;
+    //private int exp;
+    //private int lvl;
+    //private int cash;
+    //private int highscore;
     
-    public Player(String inputname) {
-        name = inputname;
+    static boolean valid = false;
+    
+    static Object Auth() {
+        valid = true;
+        do{
+        System.out.println("Melden Sie sich an:");
+        System.out.println("1. Anmelden");
+        System.out.println("2. Account Registieren");
+        System.out.println("3. Benutzer auflisten");
+        String selected = Reader.readString();
+            switch (selected) {
+                case "1":
+
+                    break;
+                case "2":
+                    System.out.println("Geben Sie Ihren Nicknamen ein:");
+                    
+                    
+                    break;
+                case "3":
+                    
+                    valid = false;
+                    break;
+                default:
+                    valid = false;
+                    System.out.println("Ungültige Eingabe");
+                    Tools.sleep(5000);
+            }
+        }
+        while(valid = true);
+        Player pl = new Player();
+        return pl;
     }
 }
